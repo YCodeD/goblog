68b9a6b9294d677f32599e75e493204835320842
yuelang <1255280233@qq.com>
2020-11-22 21:15:57 +0800

    读取请求数据

--- main.go
+++ main.go
@@ -76,10 +76,32 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request)  {
 	fmt.Fprint(w, "访问文章列表")
 }
 
+// 添加新文章
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w, "创建新的文章")
+	err := r.ParseForm()
+	if err != nil {
+		// 解析错误
+		fmt.Fprint(w, "请提供正确的数据！")
+		return
+	}
+
+	title := r.PostForm.Get("title")
+
+	fmt.Fprintf(w, "POST PostForm: %v <br>", r.PostForm)
+	fmt.Fprintf(w, "POST Form: %v <br>", r.Form)
+	fmt.Fprintf(w, "title 的值为：%v", title)
+
+
+	// 可直接使用r.FormValue r.PostFormValue 获取请求内容, 无需使用r.ParseForm
+	// fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
+    // fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+    // fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+    // fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
+
+
 }
 
+// 标头中间件
 func forceHTMLMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 1. 设置标头
@@ -89,6 +111,7 @@ func forceHTMLMiddleware(h http.Handler) http.Handler {
 	})
 }
 
+// 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request)  {
 	html := `
 <!DOCTYPE html>
@@ -97,7 +120,7 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request)  {
 	<title>创建文章 -- 我的技术博客</title>
 </head>
 <body>
-	<form action="%s" method="post">
+	<form action="%s?test=data" method="post">
 		<p><input type="text" name="title"></p>
 		<p><textarea name="body" cols="30" rows="10"></textarea></p>
 		<p><button type="submit">提交</button></p>
