38221126b0b23f8d023787bf06644498d5dd39ad
yuelang <1255280233@qq.com>
2020-11-22 21:56:49 +0800

    使用模板文件

--- main.go
+++ main.go
@@ -117,29 +117,6 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "body 的长度为: %v <br>", utf8.RuneCountInString(body))
 	} else {
 		// 对 errors 的传参到html中进行渲染，使用到标准库html/template
-		html := `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>创建文章 -- 我的技术博客</title>
-	<style type="text/css">.error {color: red;}</style>
-</head>
-<body>
-	<form action="{{ .URL }}" method="post">
-		<p><input type="text" name="title" value="{{ .Title }}"></p>
-		{{ with .Errors.title }}
-		<p class="error">{{ . }}</p>
-		{{ end }}
-		<p><textarea name="body" cols="30" rows="10">{{ .Body }}</textarea></p>
-		{{ with .Errors.title }}
-		<p class="error">{{ . }}</p>
-		{{ end }}
-		<p><button type="submit">提交</button></p>
-	</form>
-</body>
-</html>
-		`
-
 		storeURL, _ := router.Get("articles.store").URL()
 
 		data := ArticlesFormData{
@@ -149,7 +126,8 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 			Errors: errors,
 		}
 
-		tmpl, err := template.New("create-form").Parse(html)
+
+		tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
 		if err != nil {
 			panic(err)
 		}
@@ -171,24 +149,21 @@ func forceHTMLMiddleware(h http.Handler) http.Handler {
 
 //
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
-	html := `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<title>创建文章 -- 我的技术博客</title>
-</head>
-<body>
-	<form action="%s" method="post">
-		<p><input type="text" name="title"></p>
-		<p><textarea name="body" cols="30" rows="10"></textarea></p>
-		<p><button type="submit">提交</button></p>
-	</form>
-</body>
-</html>
-`
 
 	storeURL, _ := router.Get("articles.store").URL()
-	fmt.Fprintf(w, html, storeURL)
+	data := ArticlesFormData{
+		Title: "",
+		Body: "",
+		URL: storeURL,
+		Errors: nil,
+	}
+
+	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl.Execute(w, data)
 }
 
 func removeTrailingSlash(next http.Handler) http.Handler {
