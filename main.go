8864819bb5c22bb7f75043d603baf5ee379e173d
yuelang <1255280233@qq.com>
2020-11-24 09:52:59 +0800

    重构 checkError

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
+	"goblog/pkg/types"
 )
 
 var router *mux.Router
@@ -151,13 +152,6 @@ func(a Article) Delete() (rowsAffected int64, err error) {
 	return 0, nil
 }
 
-
-
-// Int64ToString 将 int64 转换为 string
-func Int64ToString(num int64) string {
-	return strconv.FormatInt(num, 10)
-}
-
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取 URL 参数
 	id := route.GetRouterVariable("id", r)
@@ -182,7 +176,7 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 		// fmt.Fprint(w, "读取成功，文章标题--" + article.Title)
 		tmpl, err := template.New("show.gohtml").Funcs(template.FuncMap{
 			"RouteName2URL": route.Name2URL,
-			"Int64ToString": Int64ToString,
+			"Int64ToString": types.Int64ToString,
 			}).ParseFiles("resources/views/articles/show.gohtml")
 		logger.LogError(err)
 		tmpl.Execute(w, article)
