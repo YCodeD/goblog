4d0aa58d5ea5ab6f7e19eabd429bd5156c882410
yuelang <1255280233@qq.com>
2020-11-24 18:15:13 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,94 +1,17 @@
 package main
 
 import (
-	"database/sql"
 	"net/http"
-	"strconv"
-	"strings"
 
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
-
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
 )
-
-var router *mux.Router
-var db *sql.DB
-
-// Article 对应一条文章数据, 用以存储从数据库里读出来的文章数据
-type Article struct {
-	Title, Body string
-	ID          int64
-}
-
-// Delete 方法用以从数据库中删除单条记录
-func (a Article) Delete() (rowsAffected int64, err error) {
-	rs, err := db.Exec("delete from articles where id = " + strconv.FormatInt(a.ID, 10))
-
-	if err != nil {
-		return 0, err
-	}
-
-	// 删除成功，跳转到文章详情页
-	if n, _ := rs.RowsAffected(); n > 0 {
-		return n, nil
-	}
-
-	return 0, nil
-}
-
-func getArticleByID(id string) (Article, error) {
-	article := Article{}
-	query := "select * from articles where id = ?"
-	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
-	return article, err
-}
-
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
-}
-
-// 标头中间件
-func forceHTMLMiddleware(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 设置标头
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		// 2. 继续处理请求
-		h.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 除首页以外，移除所有请求路径后面的斜杠
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-
-		// 2. 将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 
-	database.Initialize()
-	db = database.DB
-
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-	// 中间件：强制内容类型为 HTML
-	router.Use(forceHTMLMiddleware)
-
-	// 通过命名路由获取URL示例
-	// homeURL, _ := router.Get("home").URL()
-	// fmt.Println("homeURL:", homeURL)
-	// articlesURL, _ := router.Get("articles.show").URL("id", "23")
-	// fmt.Println("articlesURL:", articlesURL)
-
-	http.ListenAndServe(":3001", removeTrailingSlash(router))
+	http.ListenAndServe(":3001", middlewares.RemoveTrailingSlash(router))
 
 }
