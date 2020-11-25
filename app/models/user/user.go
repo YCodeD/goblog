0520209454e692ae40b321c8a5df09b2f8a9d825
yuelang <1255280233@qq.com>
2020-11-25 16:56:20 +0800

    注册表单验证

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -6,7 +6,8 @@ import "goblog/app/models"
 type User struct {
 	models.BaseModel
 
-	Name string `gorm:"column:name;type:varchar(255);not null;unique"`
-	Email string `gorm:"column:email;type:varchar(255);default:NULL;unique;"`
-	Password string `gorm:"column:name;type:varchar(255)"`
+	Name            string `gorm:"type:varchar(255);not null;unique" valid:"name"` 
+	Email           string `gorm:"type:varchar(255);unique;" valid:"email"`
+	Password        string `gorm:"type:varchar(255)" valid:"password"`
+	PasswordComfirm string `gorm:"-" valid:"password_comfirm"`
 }
