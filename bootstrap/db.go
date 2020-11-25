e3b2df2a540806fd19a7cbdad617f92e0e77b493
yuelang <1255280233@qq.com>
2020-11-25 14:43:02 +0800

    创建用户

--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,25 +1,37 @@
 package bootstrap
 
 import (
+	"goblog/app/models/article"
+	"goblog/app/models/user"
 	"goblog/pkg/model"
-	
-	
-    "time"
+
+	"time"
+
+	"gorm.io/gorm"
 )
 
 // SetupDB 初始化数据库和 ORM
 func SetupDB() {
 
-    // 建立数据库连接池
-    db := model.ConnectDB()
+	// 建立数据库连接池
+	db := model.ConnectDB()
 
-    // 命令行打印数据库请求的信息
-    sqlDB, _ := db.DB()
+	// 命令行打印数据库请求的信息
+	sqlDB, _ := db.DB()
 
-    // 设置最大连接数
-    sqlDB.SetMaxOpenConns(100)
-    // 设置最大空闲连接数
-    sqlDB.SetMaxIdleConns(25)
-    // 设置每个链接的过期时间
-    sqlDB.SetConnMaxLifetime(5 * time.Minute)
+	// 设置最大连接数
+	sqlDB.SetMaxOpenConns(100)
+	// 设置最大空闲连接数
+	sqlDB.SetMaxIdleConns(25)
+	// 设置每个链接的过期时间
+	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+	migration(db)
+}
+
+func migration(db *gorm.DB) {
+	db.AutoMigrate(
+		&user.User{},
+		&article.Article{},
+	)
 }
