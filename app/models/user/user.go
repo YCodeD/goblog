2a16ede8c9b69ebb67fea395cf095c0b5f497e26
yuelang <1255280233@qq.com>
2020-11-26 13:39:08 +0800

    用户密码加密

--- app/models/user/user.go
+++ app/models/user/user.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"goblog/app/models"
+	"goblog/pkg/password"
 )
 
 // User ?????
@@ -15,6 +16,6 @@ type User struct {
 }
 
 // ComparePassword ????????
-func (u User) ComparePassword(password string) bool {
-	return u.Password == password
+func (u User) ComparePassword(_password string) bool {
+	return password.CheckHash(_password, u.Password)
 }
